5fd8edf637937a05f48e0edc8e9f3c687b8df1b9
shalindra shrestha <shalindra.shrestha@gmail.com>
2017-11-14 14:59:45 -0800

    changed port and partial socket IO

--- app.js
+++ app.js
@@ -7,12 +7,18 @@ var session = require('express-session');
 var passport = require('passport');
 var flash = require('connect-flash');
 
+var app = express();
+
+
+//server and socketIO
+var server = require('http').createServer(app);
+var socketIO = require('socket.io').listen(server);
+
 
 var blogRouter = require('./routes/blogRoutes.js');
 var singleRouter = require('./routes/singleRoutes.js');
 var authRouter = require('./routes/authRoutes.js');
 
-var app = express();
 
 //view engine
 app.set('view engine', 'ejs');
@@ -40,7 +46,7 @@ var mongoose = require('mongoose');
 
 //connect to mongoDB
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://testUser:testPassword@ds157702.mlab.com:57702/standuptutorial', {
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://testUser:testPassword@ds157702.mlab.com:57702/standuptutorial', {
         useMongoClient: true,
          /* other options */
     });
@@ -67,13 +73,30 @@ app.use('/', blogRouter);
 app.use('/single', singleRouter);
 app.use('/auth', authRouter);
 
-app.set('port', (process.env.OPENSHIFT_NODEJS_PORT || 3000));
-app.set('port_ip', (process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1'));
+app.set('port', (process.env.PORT || 3000));
+//app.set('port_ip', (process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1'));
 
-app.listen(app.get('port'), app.get('port_ip'),function(){
+server.listen(app.get('port'), function(){
     console.log('running...');
+});
+
+var connections = [];
+//socketIO
+socketIO.on('connection', function(socket){
+    connections.push(socket);
+    console.log('connected: %s to socket', connections.length);
+
+    socket.on('disconnect', function(data){
+        connections.splice(connections.indexOf(socket), 1);
+        console.log('Disconnected: %s to socket', connections.length);
+    });
+
+    //send message
+    socket.on('send message', function(data){
+    // we tell the client to execute 'new message'
+    socketIO.emit('new message', {
+       // username: socket.username,
+        message: data
+      });
+    })
 })
-
-
-
-
