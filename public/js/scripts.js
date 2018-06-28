098358694196754a84642bd8dc8fa8dae8455704
shalindra shrestha <shalindra.shrestha@gmail.com>
2018-06-28 10:41:15 -0700

    fixed safari loading problem

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -1,18 +1,17 @@
+'use strict';
 
-(function($) {
+(function ($) {
     'use strict';
 
-    jQuery(document).ready(function() {
-
+    jQuery(document).ready(function () {
 
         /* Preloader */
-        $(window).load(function() {
+        $(window).load(function () {
             $('.preloader').delay(800).fadeOut('slow');
         });
 
-
         /* Scroll To Top */
-        $(window).scroll(function() {
+        $(window).scroll(function () {
             if ($(this).scrollTop() >= 500) {
                 $('.scroll-to-top').fadeIn();
             } else {
@@ -20,8 +19,7 @@
             }
         });
 
-
-        $('.scroll-to-top').click(function() {
+        $('.scroll-to-top').click(function () {
             $('html, body').animate({
                 scrollTop: 0
             }, 800);
@@ -31,53 +29,31 @@
         /* initialize popover */
         $('[data-toggle="popover"]').popover();
 
-        
-
         /* Load More Post */
-        $("#load-more-post").on('click', function(e) {
+        $("#load-more-post").on('click', function (e) {
             e.preventDefault();
-             let items = $('.blog-post').length;
+            var items = $('.blog-post').length;
 
             $.ajax({
                 type: "GET",
                 url: "/moreData/" + items,
-                success: function(data) {
+                success: function success(data) {
                     var obj = JSON.parse(data);
 
-                  if (obj.length == 0){
-                    $('#post-end-message').html('<div class="end">End</div>').fadeIn(800);
-                    $("#load-more-post").hide();
-                  } else {
-                    let newBlog = obj[0];
-                    var markUp = `
-                        <div class="col-md-12 blog-post" style="display:none">
-                            <div class="post-title">
-                                <a href="/single/${newBlog._id}">
-                                        <h1>${newBlog.title}</h1>
-                                </a>
-                            </div>
-                                <div class="post-info">
-                                <span>${newBlog.date}  by <span class="color-6">${newBlog.author}</span></span>
-                            </div>
-                            <p>
-                                ${newBlog.description}
-                            </p>
-                                <a href="/single/${newBlog._id}" class="button button-style button-anim fa fa-long-arrow-right"><span>Read More</span></a>
-                        </div>
-                    `;
-                    
-                     $(markUp).appendTo('.content-page').slideDown('slow');
+                    if (obj.length == 0) {
+                        $('#post-end-message').html('<div class="end">End</div>').fadeIn(800);
+                        $("#load-more-post").hide();
+                    } else {
+                        var newBlog = obj[0];
+                        var markUp = '\n                        <div class="col-md-12 blog-post" style="display:none">\n                            <div class="post-title">\n                                <a href="/single/' + newBlog._id + '">\n                                        <h1>' + newBlog.title + '</h1>\n                                </a>\n                            </div>\n                                <div class="post-info">\n                                <span>' + newBlog.date + '  by <span class="color-6">' + newBlog.author + '</span></span>\n                            </div>\n                            <p>\n                                ' + newBlog.description + '\n                            </p>\n                                <a href="/single/' + newBlog._id + '" class="button button-style button-anim fa fa-long-arrow-right"><span>Read More</span></a>\n                        </div>\n                    ';
 
-                  }
+                        $(markUp).appendTo('.content-page').slideDown('slow');
+                    }
                 },
-                error: function(jqXHR, textStatus, err) {
-                   alert('Error! ' + textStatus + err);
+                error: function error(jqXHR, textStatus, err) {
+                    alert('Error! ' + textStatus + err);
                 }
             });
-
-
         });
-
     });
-
 })(jQuery);
